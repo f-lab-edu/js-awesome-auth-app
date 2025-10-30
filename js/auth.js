7e092be30f6bea422c1678feaa714a4e8dc90169
rimiJI <hlji619@naver.com>
2025-10-30 16:32:04 +0900

    [251030] signIn기능 구현(토큰생성)

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,7 @@ class Auth {
     //초기값 기억
     //여러명 빠르게 꺼낼 수 있는 Map으로
     //사용자:키(이메일)-값(비밀번호)
-    //토큰세션:키(토큰)-값(세션)
+    //토큰:키(토큰)-값(이메일) //값(세션)이 오는게 X, session은 사용자 상태 기록 데이터 자체
     this.users = new Map(); //로그인, 가입시 사용됨.
     this.tokenSessions = new Map(); //로그인 시 생성하고 반환.
   }
@@ -23,9 +23,19 @@ class Auth {
     // TODO
     //기존에 있는 메일이면 토큰부여, true (성공)
     //기존에 없는 메일이면 또는, 비밀번호가 맞지 않으면 false (실패)
-    return { ok: true, token, user: { email } }; // 성공시
+
     // 실패시
-    return { ok: false, reason: "INVALID_PASSWORD" }; // 혹은 INVALID_EMAIL
+    //만약 아이디가 없다면?
+    if (!this.users.has(email)) return { ok: false, reason: "INVALID_EMAIL" };
+    //만약 아이디는 있는데, 비밀번호가 다르다면?
+    else if (password !== this.users.get(email))
+      return { ok: false, reason: "INVALID_PASSWORD" }; // 혹은 INVALID_EMAIL
+    // 성공시
+    //토큰생성 (Math.random() * 10는 0~9 사이 숫자)(6자리수)
+    const token = Math.floor(Math.random() * 10 * 10 ** 5);
+    //생성한 토큰을 tokenSessions에 저장
+    this.users.set(token, email);
+    return { ok: true, token, user: { email } }; // 성공시 (이걸 나중으로 빼자)
   }
 
   signOut(token) {
@@ -53,11 +63,11 @@ console.log(auth.signUp("a@ex.com", "xxxx"));
 // { ok: false, reason: "EMAIL_TAKEN" }
 // 이메일이 이미 존재 하니까
 
-/*
 const login = auth.signIn("a@ex.com", "1234");
 console.log(login);
 // { ok: true, token: 'a93f1c...', user: { email: 'a@ex.com' } }
 
+/*
 console.log(auth.verify(login.token));
 // { ok: true, user: { email: 'a@ex.com' } }
 
